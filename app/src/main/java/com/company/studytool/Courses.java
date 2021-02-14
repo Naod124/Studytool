0c8b4dc393144514dc32b1839309bbe110421bb0
mnq11 <mnq_22@yahoo.com>
2021-02-14 09:42:12 +0100

    clean Setup

--- app/src/main/java/com/company/studytool/Courses.java
+++ app/src/main/java/com/company/studytool/Courses.java
@@ -2,13 +2,19 @@ package com.company.studytool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Courses extends AppCompatActivity {
-
+Button moveToQuiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+
+        moveToQuiz=findViewById(R.id.moveToQuiz);
+        moveToQuiz.setOnClickListener(v -> startActivity(new Intent(Courses.this, Quiz.class)));
+
     }
 }
--- app/src/main/java/com/company/studytool/Register.java
+++ app/src/main/java/com/company/studytool/Register.java
@@ -145,6 +145,7 @@ public class Register extends AppCompatActivity {
                 }
             }
         });
+
     }
 
 
