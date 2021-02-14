17adb0a78d2b55c5ef867f1a4db5d54dbf2cb238
mnq11 <mnq_22@yahoo.com>
2021-02-14 14:27:50 +0100

    clean Setup

--- app/src/main/java/com/company/studytool/login.java
+++ app/src/main/java/com/company/studytool/login.java
@@ -54,17 +54,14 @@ public class login extends AppCompatActivity {
         register.setOnClickListener(v -> startActivity(new Intent(login.this, Register.class)));
 
         loginButton.setOnClickListener(v -> {
+/*
+This will add quiz to DB
+            ArrayList<Question> qList = getQlist();
+            for (int i = 0; i < qList.size(); i++) {
+                addQuiz(" Algorithms and Data Structures", "Algorithm analysis", qList.get(i), i+1);
+            }
 
-
-                Question question = new Question();
-//                question.setMainQuestion(getLine(1));
-//                question.setAlternative1(getLine(2));
-//                question.setAlternative2(getLine(3));
-//                question.setAlternative3(getLine(4));
-//                question.setRight(getLine(5));
-                addQuiz(" Algorithms and Data Structures", "Algorithm analysis", question, 2);
-
-
+*/
 
 
             user = username.getText().toString();
@@ -114,6 +111,53 @@ public class login extends AppCompatActivity {
             }
 
         });
+    }
+
+    private ArrayList getQlist() {
+        ArrayList myList = new ArrayList();
+        Question question1 = new Question("Two main measures for the efficiency of an algorithm are", "Processor and memory", "Complexity and capacity", "Data and space", "Time and space");
+        Question question2 = new Question("The time factor when determining the efficiency of algorithm is measured by", "Counting the kilobytes of algorithm", "Counting the number of statements", "Counting microseconds", "Counting the number of key operations");
+        Question question3 = new Question("The space factor when determining the efficiency of algorithm is measured by", "Counting the maximum disk space needed by the algorithm", "Counting the average memory needed by the algorithm", "Counting the minimum memory needed by the algorithm", "Counting the maximum memory needed by the algorithm");
+        Question question4 = new Question("Which of the following cases does not exist in complexity theory", "Best case ", "Worst case", "Average case", "Null case");
+        Question question5 = new Question("The Worst case occur in linear search algorithm when", " Item is somewhere in the middle of the array", "Item is not in the array at all ", " Item is the last element in the array ", "Item is the last element in the array or is not there at all");
+        Question question6 = new Question("The Average case occur in linear search algorithm", "When Item is the last element in the array or is not there at all", "When Item is the last element in the array ", "When Item is not in the array at all ", "When Item is somewhere in the middle of the array");
+        Question question7 = new Question("The complexity of the average case of an algorithm is", "Much more simpler to analyze than that of worst case", "Sometimes more complicated and some other times simpler than that of worst case", "None of them ", "Much more complicated to analyze than that of worst case");
+        Question question8 = new Question("The complexity of linear search algorithm is", "O(log n)", "O(n2)", "O(n log n) ", "O(n)");
+        Question question9 = new Question("The complexity of Binary search algorithm is", "O(n)", "O(n log n)", "O(n2)", "O(log n)");
+        Question question10 = new Question(" The complexity of Bubble sort algorithm is", "O(n log n)", "O(log n)", "O(n)", "O(n2)");
+        Question question11 = new Question("The complexity of merge sort algorithm is", "O(n)", "O(log n)", "O(n2)", "O(n log n)");
+        Question question12 = new Question(" The complexity of selection sort algorithm is", "O(log n)", "O(n)", "O(n log n)", "O(n2)");
+        Question question13 = new Question("What is the worst case running time complexity of merge sort?", "O(n2)", "O(n)", "O(log n)", "O(n log n)");
+        Question question14 = new Question("What is the best case running time complexity of quicksort?", "O(n2)", "O(n)", "O(log n)", "O(n log n)");
+        Question question15 = new Question("What is the average case running time complexity of quicksort?", "O(n2)", "O(n)", "O(log n)", "O(n log n)");
+        Question question16 = new Question("The best case running time  complexity of insertion sort algorithm is", "O(n log n)", "O(n2)", "O(log n)", "O(n)");
+        Question question17 = new Question("A sort which compares adjacent elements in a list and switches where necessary is", "selection sort ", "quick sort", "insertion sort", "bubble sort");
+        Question question18 = new Question("As part of the maintenance work, you are entrusted with the work of rearranging the library books in a shelf in proper order, at the end of each day. The ideal choice will be", "bubble sort", "Merge sort", "Selection sort", "Insertion sort");
+        Question question19 = new Question("Which among the following is the best when the list is already sorted", "Selection sort", " Merge sort", "Bubble sort", "Insertion sort");
+        Question question20 = new Question("You have a sorted array and now you are given an element to be placed in that array so that the resulting array is also sorted, the best sorting technique in this case is", " bubble sort", "merge sort", "quick sort", "Insertion sort");
+        Question question21 = new Question("The running time of quick sort largely depends on", "arrangement of elements", "size of element ", "number of inputs", "selection of pivot element ");
+        myList.add(question1);
+        myList.add(question2);
+        myList.add(question3);
+        myList.add(question4);
+        myList.add(question5);
+        myList.add(question6);
+        myList.add(question7);
+        myList.add(question8);
+        myList.add(question9);
+        myList.add(question10);
+        myList.add(question11);
+        myList.add(question12);
+        myList.add(question13);
+        myList.add(question14);
+        myList.add(question15);
+        myList.add(question16);
+        myList.add(question17);
+        myList.add(question18);
+        myList.add(question19);
+        myList.add(question20);
+        myList.add(question21);
+        return myList;
     }
 
 
@@ -168,23 +212,6 @@ public class login extends AppCompatActivity {
         RequestQueue rQueue = Volley.newRequestQueue(login.this);
         rQueue.add(request);
     }
-
-//    public String getLine(int lineNumber) throws IOException {
-//
-//        try {
-//             str = Files.lines(Paths.get(filename)).skip(lineNumber - 1).findFirst().get();
-//            System.out.println("Content at " + lineNumber + " Number:- " + str);
-//             return str;
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Toast.makeText(login.this, str, Toast.LENGTH_LONG).show();
-//
-//
-//        return null;
-//    }
 
 
 }
