9fd3f1c9982b4fa0d0e312016f445babccccfad3
mnq11 <mnq_22@yahoo.com>
2021-03-13 05:27:06 +0100

    setup the graph

--- app/src/main/java/com/company/studytool/ShowScoreActivity.java
+++ app/src/main/java/com/company/studytool/ShowScoreActivity.java
@@ -10,6 +10,10 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,33 +26,51 @@ import java.util.Scanner;
 
 
 public class ShowScoreActivity extends AppCompatActivity {
-        TextView score;
-        Button done;
+    TextView score;
+    Button done;
+    LineGraphSeries<DataPoint> series;
 
-        @SuppressLint("SetTextI18n")
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.show_score_activity);
-            score = findViewById(R.id.scoreView);
-            done = findViewById(R.id.done);
-
-            String scoreConverted = StudentDetails.getCurrentCourse();
-            System.out.println(StudentDetails.getCurrentCourse());
-            try {
-                score.setText("Your high score " + Collections.max(getScore(scoreConverted)));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
-            done.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
-
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_score_activity);
+        score = findViewById(R.id.scoreView);
+        done = findViewById(R.id.done);
+        String scoreConverted = StudentDetails.getCurrentCourse();
+        ArrayList<Integer> MyScore = null;
+        try {
+            MyScore = getScore(scoreConverted);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+int MazScore=Collections.max(MyScore);
+        score.setText("Your high score " + MazScore);
+
+
+        double y, x;
+        x = 0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph1);
+        series = new LineGraphSeries<>();
+        for (int i = 0; i < MyScore.size(); i++) {
+            x=x+1;
+            y = MyScore.get(i);
+            series.appendData(new DataPoint(x, y), true, MazScore);
+        }
+        graph.addSeries(series);
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+
 
 
     public ArrayList<Integer> getScore(String filePath) throws FileNotFoundException {
